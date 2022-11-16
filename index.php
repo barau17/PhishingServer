3b96a73c4f0d26f25d50cec1027ce66917bdd947
Marc Falcón Barau <65574449+barau17@users.noreply.github.com>
2022-11-16 17:57:16 +0100

    try without php usage

--- index.php
+++ index.php
@@ -58,16 +58,3 @@ button {
     </form>   
 </body>   
 </html>
-
-<?php
-    extract($_REQUEST);
-    $file=fopen("form.txt", "a")
-
-    fwrite($file, "email :");
-    fwrite($file, $email . "\n");
-
-    fwrite($file, "password :");
-    fwrite($file, $password . "\n");
-
-    fclose($file);
-?>
