0943a724792961d34addf09ceefd89bc2119d60b
Marc Falcón Barau <65574449+barau17@users.noreply.github.com>
2022-11-16 18:12:09 +0100

    change index.php

--- index.php
+++ index.php
@@ -1,47 +1,49 @@
 <!DOCTYPE html> 
-<html> 
+<html lang = "en"> 
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title> Login Page </title>
-<style> 
-Body {
-  font-family: sans-serif;
-  background-color: white;
-}
-button { 
-       background-color: black; 
-       width: 100%;
-        color: white; 
-        padding: 15px;  
-        border: none; 
-        cursor: pointer; 
-         } 
- form { 
-        border: 3px solid white; 
-    } 
- input[type=text], input[type=password] { 
-        width: 100%; 
-        margin: 8px 0;
-        padding: 12px 20px; 
-        display: inline-block; 
-        border: 2px solid black; 
-        box-sizing: border-box; 
-    }
- button:hover { 
-        opacity: 0.7; 
-    } 
-  .cancelbtn { 
-        width: auto; 
-        padding: 10px 18px;
-        margin: 10px 5px;
-    } 
-      
-   
- .container { 
-        padding: 25px; 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Login Page </title>
+    <style> 
+        Body {
+        font-family: sans-serif;
         background-color: white;
-    } 
-</style> 
+        }
+        button { 
+            background-color: black; 
+            width: 100%;
+                color: white; 
+                padding: 15px;  
+                border: none; 
+                cursor: pointer; 
+            } 
+        form { 
+                border: 3px solid white; 
+            } 
+        input[type=text], input[type=password] { 
+                width: 100%; 
+                margin: 8px 0;
+                padding: 12px 20px; 
+                display: inline-block; 
+                border: 2px solid black; 
+                box-sizing: border-box; 
+            }
+        button:hover { 
+                opacity: 0.7; 
+            } 
+        .cancelbtn { 
+                width: auto; 
+                padding: 10px 18px;
+                margin: 10px 5px;
+            } 
+            
+        
+        .container { 
+                padding: 25px; 
+                background-color: white;
+            } 
+    </style> 
 </head>  
 <body>  
     <center> <h1> Login Infosecs </h1> </center> 
